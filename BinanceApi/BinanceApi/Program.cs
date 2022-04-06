6fae2a33b558ba529fc66027742aeb40b438430b
Kristofers Petersons <kristofersp2@gmail.com>
2022-04-06 15:06:47 +0300

    Minor fixes

--- BinanceApi/BinanceApi/Program.cs
+++ BinanceApi/BinanceApi/Program.cs
@@ -1,35 +1,15 @@
-﻿using System;
-using WebSocketSharp;
-using Newtonsoft.Json.Linq;
-
+﻿
 namespace BinanceApi
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (WebSocket ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade"))
-            {
-
-                ws.OnMessage += Ws_OnMessage;
-                ws.Connect();
-                Console.ReadKey();
-            }
+            WebSocketService ws = new WebSocketService();
+            ws.Startup("ethusdt");
         }
-
-        private static void Ws_OnMessage(object sender, MessageEventArgs e)
-        {
-            JObject json = JObject.Parse(e.Data);
-            var price = json.SelectToken("p").ToObject<decimal>();
-            using (var db = new BinanceDbContext())
-            {
-                var crypto = new Crypto(price);
-                db.Crypto.Add(crypto);
-                db.SaveChanges();
-                Console.WriteLine(price);
-            }
-
-        }
-
     }
 }
+
+//btcusdt
+//ethusdt
---
+++ BinanceApi/BinanceApi/Services/WebSocketService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WebSocketSharp;
+using Newtonsoft.Json.Linq;
+
+namespace BinanceApi
+{
+    public class WebSocketService
+    {
+        public void Startup(string crypto)
+        {
+            using (WebSocket ws = new WebSocket($"wss://stream.binance.com:9443/ws/{crypto}@trade"))
+            {
+                ws.OnMessage += Ws_OnMessage;
+                ws.Connect();
+                Console.ReadKey();
+            }
+        }
+
+        public static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            JObject json = JObject.Parse(e.Data);
+            var price = json.SelectToken("p").ToObject<decimal>();
+            using (var db = new BinanceDbContext())
+            {
+                var crypto = new Crypto(price);
+                db.Crypto.Add(crypto);
+                db.SaveChanges();
+                Console.WriteLine(price);
+            }
+        }
+    }
+}
